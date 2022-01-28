f533d5b7c9fe3215dc4c9f93bd2b2a15c0d86102
Andy Terwilliger <andy@terwilliger1@gmail.com>
2022-01-28 13:53:27 -0600

    sessions login POST rand delete routes created, POST successfully to Postman

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -8,7 +8,33 @@ const User = require('../models/user.js');
 
 //New (login page)
 
-//delete (login route);
+sessionsRouter.post('/', (req, res) => {
+    User.findOne({
+        email: req.body.email
+    }, (error, foundUser) =>{
+        if (!foundUser){
+            res.send('User not found... try again.')
+        } else{
+            const passwordMatches = bcrypt.compareSync(req.body.password, foundUser.password)
+
+            if(passwordMatches){
+                req.session.currentUser = foundUser;
+
+                res.redirect('/')
+            } else{
+                res.send('Invalid login!')
+            }
+        }
+    })
+})
+
+//delete (logout route);
+
+sessionsRouter.delete('/', (req, res) =>{
+    req.session.destroy((error) =>{
+        res.redirect('/')
+    })
+})
 
 //create (login route)
 
