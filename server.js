40cc70418678826df19e405ba8a050d534affbe2
Kendlyn Marriott <kendlynmarriott@Kendlyns-iMac.attlocal.net>
2022-01-29 16:43:34 -0500

    json routes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const morgan = require ('morgan')
 
 const app = express();
 
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 
 const { PORT = 4000 } = process.env;
 
@@ -26,6 +26,18 @@ const db = mongoose.connection;
 db.on('error', (err) => console.log(err.message +'Is mongodb not running?'));
 db.on('connected', () => console.log('Connected!'));
 db.on('disconnected', () => console.log('MongoDB is disconnected'));
+
+
+// IMPORT JSON FILES
+const Abilities = require('./CSV/Abilities.json');
+const Datasheets_abilities = require('./CSV/Datasheets_abilities.json');
+const Datasheets_damage = require('./CSV/Datasheets_damage.json');
+const Datasheets_keywords = require('./CSV/Datasheets_keywords.json');
+const Datasheets_models = require('./CSV/Datasheets_models.json');
+const Datasheets = require('./CSV/Datasheets');
+const Factions = require('./CSV/Factions');
+
+
 
 
 //Middleware
@@ -59,6 +71,54 @@ app.use('/', routesController);
 app.use('/users', userController);
 
 app.use('/sessions', sessionsController);
+
+
+// ROUTE FOR RETRIEVING ABILITIES INFO
+app.get('/Abilities', (req, res) => {
+    res.json(Abilities);
+});
+
+// ROUTE FOR RETRIEVING DATASHEETS_ABILITIES
+app.get('/DatasheetsAbilities', (req, res) => {
+    res.json(Datasheets_abilities);
+});
+
+// ROUTE FOR RETRIEVING DATASHEETS_DAMAGE
+app.get('/DatasheetsDamage', (req, res) => {
+    res.json(Datasheets_damage);
+});
+
+// ROUTE FOR RETRIEVING DATASHEETS_KEYWORDS
+app.get('/DatasheetsKeywords', (req, res) => {
+    res.json(Datasheets_keywords);
+});
+
+// ROUTE FOR RETRIEVING DATASHEETS_MODELS
+app.get('/DatasheetsModels', (req, res) => {
+    res.json(Datasheets_models);
+});
+
+// ROUTE FOR RETRIEVING DATASHEETS
+app.get('/Datasheets', (req, res) => {
+    res.json(Datasheets);
+});
+
+// ROUTE FOR RETRIEVING FACTIONS
+app.get('/Factions', (req, res) => {
+    res.json(Factions);
+});
+
+
+
+
+
+
+
+
+
+
+
+
 //listen for PORT
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
